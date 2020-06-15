90e377f8177a72c54fcdf63914b812fac05a5f7d
Ian Scott <chesnekov@gmail.com>
2020-06-15 11:20:29 -0700

    create new Destionation constructor and instantiates 3 destinations on page load
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
-Destination Object
-  Location
-  Landmarks
-  Time of year visited
-  Notes
-  Weather
-  Favorite activity
-  Favorite food
+function Destination (location, landmark, yearVisited, favActivity, favFood) {
+  this.location = location;
+  this.landmark = landmark;
+  this.yearVisited = yearVisited;
+  this.favActivity = favActivity;
+  this.favFood = favFood;
+}
+
+Destination.prototype.summary = function() {
+  return "Want to take a vacation? Come visit " + this.location  
+}
+
+$(document).ready (function () {
+
+  //let tokyo = new Destination ("Tokyo", "Tokyo Tower", "2015", "Eating Food", "Takoyaki");
+  let paris = new Destination ();
+  let pdx = new Destination ("Portand, OR", "Voodoo Doughnuts", "2015", "Riding a Bike");
+
+  $("#tokyo-listener").click(function () {
+    let tokyo = new Destination ("Tokyo", "Tokyo Tower", "2015", "Eating Food", "Takoyaki");
+    tokyoDisplay(tokyo);
+  });
+
+  $("#pdx").click(function() {
+    $("#result").text(pdx.summary());
+  });
+});
